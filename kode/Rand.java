0db576fd8d4bf440b3e4f17f13893eb18492ce0e
JarlTuxen <jart@kea.dk>
2018-10-24 13:07:58 +0200

    opdateret printf placeholder-forklaring

--- kode/Rand.java
+++ kode/Rand.java
@@ -3,16 +3,16 @@ public class Rand
 {
    public static void main(String[] args)
    {
-      
-      
       //brug printf("formatstreng", paramater1, parameter2, ..., paramtern);
-      System.out.printf("Formatter int: %10d er tallet og decimaltallet er: %4.8f, streng: %s", 10027, 1003.14159265, "Hej!");
-      //%d er heltal, %f er decimaltal, %2.8f betyder 2 cifre før komma og 8 efter, %s er tekststreng
+      System.out.printf("Formatter int: %10d er tallet og decimaltallet er: %.4f, streng: %s", 10027, 1003.14159265, "Hej!");
+      //%d er heltal, %f er decimaltal, %.2f betyder 4 cifre efter komma, %s er tekststreng
+      //%04d sætter 4 karakterer af til tallet og foranstiller 0'er så alle pladser bruges
       System.out.println();
       int x= 42;
       double y = Math.PI;
       
-      System.out.printf("Formatter int: %010d er tallet og decimaltallet er: %4.8f", x, y);
+      //%8.2f afsætter plads til 8 karakterer, med 2 decimaler
+      System.out.printf("Formatter int: %04d er tallet og decimaltallet er: %8.2f", x, y);
       System.out.println();
       
       //konstant med antallet af tal vi skal bruge
@@ -23,10 +23,11 @@ public class Rand
       
       for (int i=0; i<ANTAL; i++)
       {
+         //tilfædigt tal mellem 0 og 1
          double r1 = Math.random();
          //næste heltal
          int r2 = ran.nextInt(100);
-         System.out.printf("Math.random(): %1.4f ran.nextInt(): %2d%n", r1, r2);
+         System.out.printf("Math.random(): %7.4f ran.nextInt(): %2d%n", r1, r2);
       }
    }
 }
